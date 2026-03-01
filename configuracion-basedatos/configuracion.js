55be18910228d96293a4c8d06bcc3745b5808a09
rafael edu mangue <edumanguerafael@gmail.com>
2026-03-01 02:04:49 +0100

    conexion de la base de datos con render

--- configuracion-basedatos/configuracion.js
+++ configuracion-basedatos/configuracion.js
@@ -1,15 +1,37 @@
 import {Sequelize} from  'sequelize'
+import sequelize from './db';
 
-//conexion a postgrestim
+//la conexion a la base de datos en post grest en render
+
+const isProduction=process.env.NODE_ENV==="production"
 
 
-const sequelize=new Sequelize('smae','postgres','1234',{
-   host:'localhost',
-   dialect:'postgres',
-   port:5432,
-   logging:console.log,
+const sequelize= isProduction ? new Sequelize(process.env.DATABASE_URL,{
+
+   dialect:"postgres",
+   protocol:"postgrest",
+   logging:false,
+   dialectOptions:{
+    ssl:{
+      require:true,
+      rejectUnauthorized:false,
+    }
+   }
  
-  });
+  })
+
+  : new sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host:process.env.DB_HOST,
+      dialect: "postgrest",
+      port:process.env.DB_PORT,
+
+    }
+  );
+
 
 
 
